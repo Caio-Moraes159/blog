ae209f794987754d8e43921411ce2b11575a9022
Caio Moraes <caiomoraes858@gmail.com>
2025-02-07 11:15:45 -0300

    Criando e adionando comentarios

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
+    #quando o post for excluído, os comentários tbm serão
+    has_many :comments, dependent: :destroy 
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
-  get "up" => "rails/health#show", as: :rails_health_check
+  # /posts/1/comments/2
+  resources :posts do
+    resources :comments
+  end
   get '/home', controller: 'home', action: 'index'
   # get "/estudantes", to: "estudante#index", as: "estudantes"
 
